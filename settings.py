ca99b610765c5a58eb1bedd466897b1ee6935782
pvt07 <pvt0701@gmail.com>
2022-06-22 16:59:27 +0300

    New Changes 0.2

--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ class Settings:
         """Ініціалізувати налаштування гри"""
         #Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230,230,230)
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
