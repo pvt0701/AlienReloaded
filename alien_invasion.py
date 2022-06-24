d4a1f88b6054e0d36f5bfd667d820f1140179deb
pvt07 <pvt0701@gmail.com>
2022-06-24 18:01:31 +0300

    New Changes 0.5.1 Refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,15 +25,23 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
+                    _check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
+                    _check_keyup_events(event)
+
+        def _check_keydown_events(event):
+            """Реагувати на натискання клавіш"""
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+
+        def _check_keyup_events(event):
+            """Реагувати, коли клавіша не натиснута"""
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = False
 
         def _update_screen():
             """Оновити зображення на екрані та перемкнутись на новий екран"""
